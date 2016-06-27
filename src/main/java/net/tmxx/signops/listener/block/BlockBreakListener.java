01e31d2c4b0ec3143d35a3beea0850448cc95a26
contact@dev-tmxx.net <contact@dev-tmxx.net>
2016-06-27 12:40:38 +0200

    Added comments

--- src/main/java/net/tmxx/signops/listener/block/BlockBreakListener.java
+++ src/main/java/net/tmxx/signops/listener/block/BlockBreakListener.java
@@ -1,3 +1,20 @@
+/**
+ * SignOps adds new sign operations to the game.
+ * Copyright (C) 2016  tmxx
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.tmxx.signops.listener.block;
 
 import lombok.RequiredArgsConstructor;
@@ -9,6 +26,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
 /**
+ * <p>
+ *     Listener listening for the {@link org.bukkit.event.block.BlockBreakEvent}
+ *     and removes (if necessary) the operation sign.
+ * </p>
+ *
  * @author tmxx
  * @version 1.0
  */
@@ -19,6 +41,11 @@ public class BlockBreakListener implements Listener {
      */
     private final SignOps signOps;
 
+    /**
+     * Invoked when a player breaks a block.
+     *
+     * @param event The posted event.
+     */
     @EventHandler
     public void onBlockBreak( BlockBreakEvent event ) {
         if ( event.getBlock().getState() instanceof Sign ) {
